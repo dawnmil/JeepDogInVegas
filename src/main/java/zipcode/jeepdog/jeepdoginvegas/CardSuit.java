e9828a5686ff976349cdf5cf08ed22858d1a23b5
Pablo Cano <pcano@Zips-MacBook-Pro-98.local>
2015-09-23 17:12:49 -0400

    Java Casino files, Card class and CardSuit enum and CardValue

---
+++ src/main/java/zipcode/jeepdog/jeepdoginvegas/CardSuit.java
@@ -0,0 +1,12 @@
+package zipcode.jeepdog.jeepdoginvegas;
+
+/**
+ * Created by pcano on 9/22/15.
+ * zipcoder.jeepdog.jeepdoginvegas.Card Suit
+ */
+
+
+public enum CardSuit {
+
+    HEARTS, DIAMONDS, SPADES, CLUBS
+}
---
+++ src/main/java/zipcode/jeepdog/jeepdoginvegas/CardValue.java
@@ -0,0 +1,13 @@
+package zipcode.jeepdog.jeepdoginvegas;
+
+/**
+ * Created by pcano on 9/22/15.
+ * Enum of card values
+ */
+
+
+public enum CardValue {
+
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING,
+
+}
