41a02d3e779b9b00afc1c64b59153ca17a42e5b8
Dawn Milnamow <dawnMil.ZCW.de@gmail.com>
2015-09-24 15:18:29 -0400

    Completed Feature 18 (PlayerNames enum)

---
+++ src/main/java/zipcode/jeepdog/jeepdoginvegas/PlayerNames.java
@@ -0,0 +1,10 @@
+package zipcode.jeepdog.jeepdoginvegas;
+
+/**
+ * Created by dmilnamow on 9/24/15.
+ */
+public enum PlayerNames {
+    CLIFFORD, LASSIE, BUDDY, ODIE, BEETHOVEN, SCOOBY, SNOOPY, ZERO, TOTO, MILO,
+    EINSTEIN, SKIP, BENJI, SHILOH, MAMA_MIA, SLINK, CHANCE, SHADOW, BINGO, PETEY, BAXTER,
+    THE_GREAT_BAMBINO
+}
