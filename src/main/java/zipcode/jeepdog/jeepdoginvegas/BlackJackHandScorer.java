fc3ef282a8ae2e4404058b126699ce28be30ed9c
Dawn Milnamow <dawnMil.ZCW.de@gmail.com>
2015-09-24 15:10:13 -0400

    Added doc blocks to Feature 07 (BlackJackHandScorer)

--- src/main/java/zipcode/jeepdog/jeepdoginvegas/BlackJackHandScorer.java
+++ src/main/java/zipcode/jeepdog/jeepdoginvegas/BlackJackHandScorer.java
@@ -5,15 +5,28 @@ package zipcode.jeepdog.jeepdoginvegas;
  */
 public class BlackJackHandScorer {
 
-
+    /**
+     * playerScore constructor
+     * @param hand  takes a hand to be scored based on the card score values in the hand
+     * @return if score is greater than 21, bust score is 0
+     */
     public int playerScore(Hand hand) {
 
-
+        /**
+         * If hand has no cards, return score of 0
+         */
        if(hand.getNumberOfCards() == 0){ return 0; }
 
+        /**
+         * Ask if hand has an Ace to determine how much it adds to the score (1 or 11)
+         * scoreCounter starts at 0 and adds # of points based on worth of cards in the hand
+         */
         boolean hasAce = false;
         int scoreCounter = 0;
 
+        /**
+         *scoreCounter values to be added to total score as per card added to the hand
+         */
         for(Card card : hand.getCards()){
             switch (card.getValue()){
                 case ACE: hasAce = true;
@@ -43,14 +56,22 @@ public class BlackJackHandScorer {
                     break;
                 case KING: scoreCounter += 10;
                     break;
+                default:
+                    break;
             }
         }
 
-        if(hasAce = true && scoreCounter < 12) {
+        /**
+         * If the score is less than 12 when an Ace is present in the hand, 10 points will be added
+         * and Ace value is thus responsible for 11 points toward the hand's score
+         */
+        if(hasAce && scoreCounter < 12) {
             scoreCounter += 10;
         }
 
-
+        /**
+         * Bust happens after hand score surpasses 21, returns a score of 0
+         */
             return scoreCounter > 21? 0 : scoreCounter;
     }
 
