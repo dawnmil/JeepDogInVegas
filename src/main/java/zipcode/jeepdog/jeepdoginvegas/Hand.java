0e43aebc70aa7efee8f4618845981aa0d83005b6
Pablo Cano <pcano@Zips-MacBook-Pro-98.local>
2015-09-24 15:51:09 -0400

    Completed Feature10 adding my Hand and Hand Test classes.

---
+++ src/main/java/zipcode/jeepdog/jeepdoginvegas/Hand.java
@@ -0,0 +1,45 @@
+package zipcode.jeepdog.jeepdoginvegas;
+
+/**
+ * Created by pcano on 9/24/15.
+ */
+public class Hand extends CardCollection{
+
+    private Player player;
+    private  int score;
+
+
+    /**
+     * Hand constructor
+     */
+    public Hand(Player player){
+        super(null);
+        this.player = player;
+        score = 0;
+    }
+
+    /**
+     * getPlayer method
+     * @return the player
+     */
+    public Player getPlayer(){
+        return player;
+    }
+
+    /**
+     * setScore method
+     * Sets the score with an int value passed on to it.
+     * @param value
+     */
+    public void setScore(int value){
+        score  = value;
+    }
+
+    /**
+     * getScore method
+     * @return score
+     */
+    public int getScore(){
+        return score;
+    }
+}
